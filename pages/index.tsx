0490b9f7dbe13424b1743f6ec2144302e8b0f209
jikor1st <jikor1st@naver.com>
2022-09-25 02:54:45 +0900

    tttt

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
-import { Seo } from '@/extendsComponents';
 export default function MainPage() {
   return (
     <>
-      <Seo
+      {/* <Seo
         title="지기역"
         siteName="이력서"
         description="프론트엔드 개발자 지기역 이력서"
         ogImage="https://jikor1st.github.io/assets/og/resume-og.png"
         ogUrl="https://jigi-scripts.vercel.app"
-      />
+      /> */}
     </>
   );
 }
--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,8 +1,6 @@
 import { ReactElement } from 'react';
 import styled from '@emotion/styled';
 
-import { Seo } from '@/extendsComponents';
-
 import {
   ResumePageLayout,
   Headline,
@@ -18,22 +16,13 @@ const SContainer = styled.div(() => {
 
 export default function ResumePage() {
   return (
-    <div>
-      <Seo
-        title="지기역"
-        siteName="이력서"
-        description="프론트엔드 개발자 지기역 이력서"
-        ogImage="https://jikor1st.github.io/assets/og/resume-og.png"
-        ogUrl="https://jigi-scripts.vercel.app"
-      />
-      <SContainer>
-        <Headline />
-        <Introduction />
-        <Technology />
-        <Experience />
-        <ContactAndOperate />
-      </SContainer>
-    </div>
+    <SContainer>
+      <Headline />
+      <Introduction />
+      <Technology />
+      <Experience />
+      <ContactAndOperate />
+    </SContainer>
   );
 }
 
